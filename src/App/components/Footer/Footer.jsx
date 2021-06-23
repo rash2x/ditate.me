b73b326d349661f3a1b722507c48b570fe28e44c
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-23 12:59:54 +0300

    Styled footer

--- src/App/components/Footer/Footer.jsx
+++ src/App/components/Footer/Footer.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
 
 import RecentActorsIcon from '@material-ui/icons/RecentActors';
 import SelfImprovementIcon from '@material-ui/icons/SelfImprovement';
 
+const StyledFooter = styled(BottomNavigation)`
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+`;
+
 const Footer = () => {
   const [value, setValue] = React.useState(0);
 
   return (
-    <BottomNavigation
+    <StyledFooter
       value={value}
       onChange={(event, newValue) => {
         setValue(newValue);
@@ -17,7 +24,7 @@ const Footer = () => {
     >
       <BottomNavigationAction component={Link} to="/" icon={<RecentActorsIcon />} />
       <BottomNavigationAction component={Link} to="/practices" icon={<SelfImprovementIcon />} />
-    </BottomNavigation>
+    </StyledFooter>
   );
 };
 
