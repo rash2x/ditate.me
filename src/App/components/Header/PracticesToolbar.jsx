9d7944da56d15fea72248f866d300ba4e4393e92
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-23 14:24:28 +0300

    Done with header mark up (usure about components)

--- src/App/components/Header/PracticesToolbar.jsx
+++ src/App/components/Header/PracticesToolbar.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { IconButton, styled, Toolbar, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import { BottomNavigation, BottomNavigationAction, Toolbar, Typography } from '@material-ui/core';
 
 import MapIcon from '@material-ui/icons/Map';
 import ViewAgendaIcon from '@material-ui/icons/ViewAgenda';
 
-const StyledTitle = styled(Typography)``;
+const StyledTitle = styled(Typography).attrs({
+  variant: 'h2',
+})`
+  font-size: 18px;
+`;
+
+const StyledToolbar = styled(Toolbar)`
+  justify-content: space-between;
+`;
+
+const Button = styled(BottomNavigationAction)`
+  min-width: 50px;
+`;
 
 const PracticesToolbar = () => {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <Toolbar>
+    <StyledToolbar>
       <StyledTitle>Расписание практик</StyledTitle>
-      <IconButton>
-        <ViewAgendaIcon />
-      </IconButton>
-      <IconButton>
-        <MapIcon />
-      </IconButton>
-    </Toolbar>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <Button icon={<ViewAgendaIcon />} />
+        <Button disabled icon={<MapIcon />} />
+      </BottomNavigation>
+    </StyledToolbar>
   );
 };
 
