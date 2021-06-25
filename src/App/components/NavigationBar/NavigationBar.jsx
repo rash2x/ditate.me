2b1c9c107cf83bf0e84ac16a8cb266d41bc9779d
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-25 13:56:00 +0300

    added FilterIcon

--- src/App/components/NavigationBar/NavigationBar.jsx
+++ src/App/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction, Fab } from '@material-ui/core';
 
-import RecentActorsIcon from '@material-ui/icons/RecentActors';
-import SelfImprovementIcon from '@material-ui/icons/SelfImprovement';
+import { useRouter } from '../../hooks/useRouter';
+
+import { RecentActors, SelfImprovement, FilterList } from '@material-ui/icons';
 
 const Base = styled(BottomNavigation)`
   position: fixed;
@@ -12,26 +13,48 @@ const Base = styled(BottomNavigation)`
   bottom: 0;
 `;
 
-const NavigationBar = () => {
-  const [value, setValue] = React.useState(0);
+const FilterIcon = styled(Fab).attrs({
+  color: 'primary',
+  size: 'medium',
+})`
+  position: absolute;
+  width: 56px;
+  height: 56px;
+  right: 159px;
+  bottom: 28px;
+  background: ${props => props.theme.palette.primary.main};
+  box-shadow: 0px 9px 12px rgba(0, 0, 0, 0.14), 0px 3px 16px rgba(0, 0, 0, 0.12),
+    0px 5px 6px rgba(0, 0, 0, 0.2);
+  border-radius: 28px;
+`;
 
-  const handleChange = (e, newValue) => {
-    setValue(newValue);
+const NavigationBar = () => {
+  const router = useRouter();
+  let activeLink = router.pathname === '/practices' ? 1 : 0;
+  const [value, setValue] = React.useState(activeLink);
+
+  const handleChange = () => {
+    if (router.pathname === '/practices') {
+      setValue(prev => prev - 1);
+    } else {
+      setValue(prev => prev + 1);
+    }
   };
+
   return (
     <Base showLabels value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Минибудды"
-        component={Link}
-        to="/"
-        icon={<RecentActorsIcon />}
-      />
+      <BottomNavigationAction label="Минибудды" component={Link} to="/" icon={<RecentActors />} />
       <BottomNavigationAction
         label="Практики"
         component={Link}
         to="/practices"
-        icon={<SelfImprovementIcon />}
+        icon={<SelfImprovement />}
       />
+      {router.pathname === '/practices' && (
+        <FilterIcon>
+          <FilterList />
+        </FilterIcon>
+      )}
     </Base>
   );
 };
