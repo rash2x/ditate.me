b5e07c9875f3cb1d02c7c399167a73253d4d5cd9
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-27 23:22:33 +0300

    Practices Page fix

--- src/App/pages/Practices/Practices.jsx
+++ src/App/pages/Practices/Practices.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { Container } from '@material-ui/core';
 import PracticeCard from '../../components/PracticeCard.jsx';
 
 const Base = styled(Container)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
   padding-top: ${props => props.theme.mixins.toolbar.minHeight + 16}px;
+
+  @media (max-width: 480px) {
+    justify-content: center;
+  }
 `;
 
 const Practices = () => {
   return (
-    <Base component={Link} to="practiceID">
+    <Base>
       <PracticeCard />
     </Base>
   );
