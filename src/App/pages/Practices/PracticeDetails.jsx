9836061d3a7e2ffdc537d566c0b20d3c08aa81a1
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-27 23:23:36 +0300

    PracticeDetails markUp

--- src/App/pages/Practices/PracticeDetails.jsx
+++ src/App/pages/Practices/PracticeDetails.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import PracticeName from '../../components/PracticeName';
 
-import { ArrowBack } from '@material-ui/icons';
-import { IconButton, Typography } from '@material-ui/core';
+import { ArrowBack, Place, EventAvailable } from '@material-ui/icons';
+import { Container, IconButton, Typography } from '@material-ui/core';
 
-import Teacher from '../../../assets/temp/teacher.jpg';
+import Contact from '../../components/Contact';
 import { useRouter } from '../../hooks/useRouter';
+import Teacher from '../../../assets/temp/teacher.jpg';
 
 const BackgroundDiv = styled.div`
   min-height: 375px;
@@ -32,7 +33,38 @@ const ArrowWrapper = styled(IconButton)`
 const Subtitle = styled(Typography)`
   margin-top: ${props => props.theme.spacing(1)}px;
 `;
-const Wrapper = styled.div``;
+
+const Info = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: ${props => props.theme.spacing(1)}px;
+`;
+
+const Date = styled(Typography)`
+  display: flex;
+  align-items: flex-end;
+  color: ${props => props.theme.palette.primary.main};
+
+  .MuiSvgIcon-root {
+    margin-right: ${props => props.theme.spacing(1)}px;
+  }
+`;
+
+const Price = styled(Typography)`
+  display: flex;
+  align-items: center;
+  padding: 2px 4px;
+  border-radius: 4px;
+  background: rgba(21, 22, 19, 1);
+`;
+
+const Location = styled(Typography)`
+  display: flex;
+  margin: ${props => props.theme.spacing(3)}px 0;
+  .MuiSvgIcon-root {
+    margin-right: ${props => props.theme.spacing(3)}px;
+  }
+`;
 
 const PracticeDetails = () => {
   const router = useRouter();
@@ -42,17 +74,34 @@ const PracticeDetails = () => {
   };
 
   return (
-    <BackgroundDiv>
-      <ArrowWrapper onClick={handleClick}>
-        <ArrowBack />
-      </ArrowWrapper>
-      <Wrapper>
-        <PracticeName />
-        <Subtitle variant="subtitle2" component="h2">
-          Даосские практики с Константином Сухановым
-        </Subtitle>
-      </Wrapper>
-    </BackgroundDiv>
+    <div>
+      <BackgroundDiv>
+        <ArrowWrapper onClick={handleClick}>
+          <ArrowBack />
+        </ArrowWrapper>
+        <div>
+          <PracticeName />
+          <Subtitle variant="subtitle2" component="h2">
+            Даосские практики с Константином Сухановым
+          </Subtitle>
+        </div>
+      </BackgroundDiv>
+      <Container>
+        <Info>
+          <Date variant="subtitle2" component="span">
+            <EventAvailable />
+            Сегодня в 18:00
+          </Date>
+          <Price variant="subtitle2" component="span">
+            1000 ₽
+          </Price>
+        </Info>
+        <Location>
+          <Place /> Конюшенная пл. 2В, 3 этаж, помещение 27
+        </Location>
+        <Contact />
+      </Container>
+    </div>
   );
 };
 
