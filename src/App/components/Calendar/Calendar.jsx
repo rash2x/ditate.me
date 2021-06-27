d0c0132dea3056fc111a9dfe02cb1b97f764283c
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-27 18:20:45 +0300

    calendar set up (styling needed)

---
+++ src/App/components/Calendar/Calendar.jsx
@@ -0,0 +1,39 @@
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+import styled from 'styled-components';
+
+const Base = styled(KeyboardDatePicker).attrs({
+  fullWidth: true,
+})`
+  .MuiIconButton-root {
+    color: rgba(0, 0, 0, 0.54);
+  }
+`;
+
+const Calendar = () => {
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <Base
+          id="date-picker-dialog"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+};
+
+export default Calendar;
