59ac422b6f93fdc75367033fa55df593e4f422f6
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-27 23:42:14 +0300

    PracticeDetails fixes

--- src/App/pages/Practices/PracticeDetails.jsx
+++ src/App/pages/Practices/PracticeDetails.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import PracticeName from '../../components/PracticeName';
 
+import { Container, Fab, Typography } from '@material-ui/core';
 import { ArrowBack, Place, EventAvailable } from '@material-ui/icons';
-import { Container, IconButton, Typography } from '@material-ui/core';
 
 import Contact from '../../components/Contact';
 import { useRouter } from '../../hooks/useRouter';
@@ -17,18 +17,12 @@ const BackgroundDiv = styled.div`
   padding: ${props => props.theme.spacing(2)}px;
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.85) 65.62%),
     url(${Teacher}) no-repeat center center/cover;
-  background-color: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.85) 65.62%);
 `;
 
-const ArrowWrapper = styled(IconButton)`
-  width: 48px;
-  height: 48px;
-  background: ${props => props.theme.palette.primary.main};
-  box-shadow: 0px 9px 12px rgb(0 0 0 / 14%), 0px 3px 16px rgb(0 0 0 / 12%),
-    0px 5px 6px rgb(0 0 0 / 20%);
-  border-radius: 24px;
-  color: black;
-`;
+const BackButton = styled(Fab).attrs({
+  color: 'primary',
+  size: 'medium',
+})``;
 
 const Subtitle = styled(Typography)`
   margin-top: ${props => props.theme.spacing(1)}px;
@@ -76,9 +70,9 @@ const PracticeDetails = () => {
   return (
     <div>
       <BackgroundDiv>
-        <ArrowWrapper onClick={handleClick}>
+        <BackButton onClick={handleClick}>
           <ArrowBack />
-        </ArrowWrapper>
+        </BackButton>
         <div>
           <PracticeName />
           <Subtitle variant="subtitle2" component="h2">
