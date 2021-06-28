34fff4c09218460e0fe721d9369822c9a0abeb68
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-06-28 12:22:03 +0300

    calendar changes

--- src/App/components/Calendar/Calendar.jsx
+++ src/App/components/Calendar/Calendar.jsx
@@ -4,26 +4,65 @@ import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import styled from 'styled-components';
+import { TextField } from '@material-ui/core';
 
 const Base = styled(KeyboardDatePicker).attrs({
   fullWidth: true,
+  variant: 'outlined',
 })`
   .MuiIconButton-root {
     color: rgba(0, 0, 0, 0.54);
   }
 `;
 
-const Calendar = () => {
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+const Input = styled(TextField).attrs({
+  variant: 'outlined',
+  margin: 'normal',
+  fullWidth: true,
+})`
+  .MuiOutlinedInput-notchedOutline,
+  .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline,
+  .Mui-focused,
+  .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline {
+    border-color: ${props => props.theme.palette.background.paper};
+  }
+
+  .MuiInputBase-root {
+    color: black;
+  }
+
+  .MuiOutlinedInput-input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 100px #fff59d inset;
+    -webkit-text-fill-color: black;
+    caret-color: black;
+  }
+
+  .MuiFormHelperText-root:not(.Mui-error),
+  .MuiFormLabel-root:not(.Mui-error) {
+    color: inherit;
+  }
+`;
+
+const Calendar = ({ setValue }) => {
+  const [selectedDate, setSelectedDate] = React.useState(null);
 
   const handleDateChange = date => {
     setSelectedDate(date);
+    setValue('date', date);
   };
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Grid container justify="space-around">
         <Base
+          TextFieldComponent={props => <Input {...props} />}
+          autoOk={true}
+          disableFuture={true}
+          emptyLabel="Выберите дату"
+          invalidDateMessage="Не верная дата"
+          maxDateMessage="Нельзя выбрать дату после текущего дня"
+          minDateMessage="Нельзя выбрать дату до минимального значения"
+          format="dd/MM/yyyy"
           id="date-picker-dialog"
           value={selectedDate}
           onChange={handleDateChange}
