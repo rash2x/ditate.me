51a42bc4bf46c5dd54bf7b0346259b568c9fa331
Ramazan Ittiev <ramazzan.ittiev@gmail.com>
2021-07-07 22:47:12 +0300

    profile arrow back fix

--- src/App/pages/Profile/Profile.jsx
+++ src/App/pages/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 
 import styled from 'styled-components';
-import { Chip, Container, Fab, fade, Typography } from '@material-ui/core';
+import { Chip, Container, fade, Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 import { ArrowBack } from '@material-ui/icons';
@@ -63,8 +63,7 @@ const CommingEvents = styled(Typography).attrs({ variant: 'h2' })`
   margin-top: ${props => props.theme.spacing(4)}px;
 `;
 
-const BackButton = styled(Fab).attrs({
-  color: 'primary',
+const BackButton = styled(ArrowBack).attrs({
   size: 'medium',
 })`
   position: absolute;
@@ -107,6 +106,10 @@ const StyledContact = styled(Contact)`
   margin-top: 32px;
 `;
 
+const Copyright = styled.div`
+  margin: ${props => props.theme.spacing(2)}px auto;
+`;
+
 const Profile = () => {
   const [state, dispatch] = useContext(AirtableContext);
   const [currentTeacher, setCurrentTeacher] = useState(null);
@@ -117,13 +120,10 @@ const Profile = () => {
 
   useEffect(() => {
     setCurrentTeacher(getTeacherById(teacherId, state.teachers));
-  }, [dispatch, state.teachers, teacherId]);
-
-  useEffect(() => {
-    if (currentTeacher !== null) {
+    if (currentTeacher) {
       currentTeacher.events.map(eventId => setEvents(getEventById(eventId, state.events)));
     }
-  }, [dispatch]);
+  }, [dispatch, state.teachers, teacherId]);
 
   const handleClick = () => {
     if (router.history.length === 1) {
@@ -132,65 +132,68 @@ const Profile = () => {
       router.history.goBack();
     }
   };
+
   return currentTeacher ? (
     <Base>
-      <BackButton onClick={handleClick}>
-        <ArrowBack />
-      </BackButton>
-      <Info>
-        <Image src={currentTeacher.image} alt="" />
-        <Name>{currentTeacher.name}</Name>
-        <Instagram
-          component="a"
-          href={currentTeacher.instagramUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={InstagramIcon} width="24px" alt="In" />
-          {currentTeacher.instagram}
-        </Instagram>
-        <Description>{currentTeacher.description}</Description>
-      </Info>
+      <BackButton onClick={handleClick} />
+      <div>
+        <Info>
+          <Image src={currentTeacher.image} alt="" />
+          <Name>{currentTeacher.name}</Name>
+          <Instagram
+            component="a"
+            href={currentTeacher.instagramUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={InstagramIcon} width="24px" alt="In" />
+            {currentTeacher.instagram}
+          </Instagram>
+          <Description>{currentTeacher.description}</Description>
+        </Info>
 
-      <PracticeList>
-        {currentTeacher.practiceIds.map(practiceId => {
-          const practice = getPracticeById(practiceId, state.practices);
+        <PracticeList>
+          {currentTeacher.practiceIds.map(practiceId => {
+            const practice = getPracticeById(practiceId, state.practices);
 
-          return (
-            practice && (
-              <PracticeChip
-                key={practiceId}
-                style={{
-                  color: practice.color,
-                  backgroundColor: fade(practice.color, 0.12),
-                }}
-                label={practice.name}
-              />
-            )
-          );
-        })}
-      </PracticeList>
+            return (
+              practice && (
+                <PracticeChip
+                  key={practiceId}
+                  style={{
+                    color: practice.color,
+                    backgroundColor: fade(practice.color, 0.12),
+                  }}
+                  label={practice.name}
+                />
+              )
+            );
+          })}
+        </PracticeList>
 
-      {currentTeacher.events && <CommingEvents>Ближайшие практики</CommingEvents> && (
-        <EventsList>
-          {new Date(events.startDate).getTime() > new Date().getTime() && (
-            <PracticeCard key={eventId} {...events} />
-          )}
-        </EventsList>
-      )}
-      <StyledContact
-        hands={HoldingHands}
-        contact={{
-          type: currentTeacher.telegram ? 'telegram' : 'instagram',
-          value: currentTeacher.telegram || currentTeacher.instagram,
-        }}
-        description={
-          <>
-            Сообщите, пожалуйста, что вы нашли меня на <span>ditate.me 🙏</span>
-          </>
-        }
-      />
-
+        {currentTeacher.events && <CommingEvents>Ближайшие практики</CommingEvents> && (
+          <EventsList>
+            {new Date(events.startDate).getTime() > new Date().getTime() && (
+              <PracticeCard key={eventId} {...events} />
+            )}
+          </EventsList>
+        )}
+        <StyledContact
+          hands={HoldingHands}
+          contact={{
+            type: currentTeacher.telegram ? 'telegram' : 'instagram',
+            value: currentTeacher.telegram || currentTeacher.instagram,
+          }}
+          description={
+            <>
+              Сообщите, пожалуйста, что вы нашли меня на <span>ditate.me 🙏</span>
+            </>
+          }
+        />
+      </div>
+      <Copyright>
+        2021 &copy; <strong>#Минибудды</strong> обучают 👌
+      </Copyright>
       <Helmet>
         <meta name="description" content={currentTeacher.description} />
         <title>
